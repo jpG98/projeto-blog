066edb88cfdb4e5ec3220a9ddc6c6923f7f45720
1º ETIM - Desenvolvimento de Sistemas <ds-etim-1@PC127.ACADEMICO>
2022-10-20 15:26:10 -0300

    começo

---
+++ js/funcoes.js
@@ -0,0 +1,10 @@
+//Cria uma variável para manipular um arquivo XML
+xmlhttp = new XMLHttpRequest();
+//le um determinado arquivo XMl no diretório do prejeto
+xmlhttp.open("GET","xml/conteudo.xml",false);
+//Envia o arquivo aberto para leitura
+xmlhttp.send();
+//Envia uma resposta do tipo XML
+xmldoc = xmlhttp.responseXMl;
+
+x = xmldoc.getElementsBytagName("postagem");
