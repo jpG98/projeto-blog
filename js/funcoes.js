4e5f92911bdb50b164ba1365db1ef32363d66fc5
1º ETIM - Desenvolvimento de Sistemas <ds-etim-1@PC127.ACADEMICO>
2022-10-27 15:23:08 -0300

    3

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,23 +1,23 @@
 //Cria uma variável para manipular um arquivo XML
 xmlhttp = new XMLHttpRequest();
-//le um determinado arquivo XMl no diretório do prejeto
+//Lê um determinado arquivo XML no diretório do projeto
 xmlhttp.open("GET","xml/conteudo.xml",false);
 //Envia o arquivo aberto para leitura
 xmlhttp.send();
 //Envia uma resposta do tipo XML
 xmlDoc = xmlhttp.responseXML;
-//Cria um array a partir da quantidade de postagems
+//Cria um array a partir da quantidade de postagens
 x = xmlDoc.getElementsByTagName("postagem");
 
+
 function funcaoConteudo(){
-    for(i=0; i<=2; i++){
-        document.write ( 
-            "<tr>"+ 
-                "<td>" + x[i].getAttribute("codigo") + "</td>" +
-                "<td> <img src='imgs/" + x[i].getElementsByTagName("imagen")[0].chlIdModes[0].modeValue + "' width = '150' ></td>"+
-                "<td>" + x[i].getElementsByTagName("titulo")[0].chlIdModes[0].modeValue + "</td>"+
-                "<td>" + x[i].getAttribute("codigo") + "</td>"+
-            "<tr>"
-        );
+    for(i=0;i<=2;i++){
+        document.write(
+        "<tr>" +
+            "<td>"+ x[i].getAttribute("codigo") +"</td>" +
+            "<td><img src='imgs/"+ x[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"' width='150'></td>" +
+            "<td>"+ x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</td>" +
+            "<td>"+ x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,150) +"...</td>" +
+        "</tr>");
     }
 }
