2e7f35087cceab35e5fcce24450b5d232fcf3f4d
1º ETIM - Desenvolvimento de Sistemas <ds-etim-1@PC127.ACADEMICO>
2022-10-27 13:24:29 -0300

    2

--- js/funcoes.js
+++ js/funcoes.js
@@ -5,18 +5,19 @@ xmlhttp.open("GET","xml/conteudo.xml",false);
 //Envia o arquivo aberto para leitura
 xmlhttp.send();
 //Envia uma resposta do tipo XML
-xmldoc = xmlhttp.responseXMl;
+xmlDoc = xmlhttp.responseXML;
 //Cria um array a partir da quantidade de postagems
-x = xmldoc.getElementsBytagName("postagem");
+x = xmlDoc.getElementsByTagName("postagem");
 
 function funcaoConteudo(){
-    for(i=1; i<=10; i++){
+    for(i=0; i<=2; i++){
         document.write ( 
             "<tr>"+ 
-                "<td> A </td>"+
-                "<td> B </td>"+
-                "<td> C </td>"+
-                "<td> D </td>"+
-            "<tr>");
+                "<td>" + x[i].getAttribute("codigo") + "</td>" +
+                "<td> <img src='imgs/" + x[i].getElementsByTagName("imagen")[0].chlIdModes[0].modeValue + "' width = '150' ></td>"+
+                "<td>" + x[i].getElementsByTagName("titulo")[0].chlIdModes[0].modeValue + "</td>"+
+                "<td>" + x[i].getAttribute("codigo") + "</td>"+
+            "<tr>"
+        );
     }
 }
